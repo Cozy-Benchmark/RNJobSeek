3ad92a75c54ae0fef54364566b97a32e8758622b
Bryan Benchmark Mendoza <139943559+Cozy-Benchmark@users.noreply.github.com>
2023-12-12 15:00:27 +0800

    Developed a hook for fetching data from rapid api

--- components/home/popular/Popularjobs.jsx
+++ components/home/popular/Popularjobs.jsx
@@ -11,18 +11,21 @@ import { useRouter } from "expo-router";
 import styles from "./popularjobs.style";
 import { COLORS, SIZES } from "../../../constants";
 import PopularJobCard from "../../common/cards/popular/PopularJobCard";
+import useFetch from "../../../hook/useFetch";
 
 const Popularjobs = () => {
   const router = useRouter();
-  const isLoading = false;
-  const error = false;
+  const { data, isLoading, error } = useFetch("search", {
+    query: "React developer",
+    num_pages: 1,
+  });
 
   return (
     <View style={styles.container}>
       <View style={styles.header}>
-        <Text style={styles.headerTitle}>Popularjobs</Text>
-        <TouchableOpacity style={styles.headerBtn}>
-          <Text>Show All</Text>
+        <Text style={styles.headerTitle}>Popular Jobs</Text>
+        <TouchableOpacity>
+          <Text style={styles.headerBtn}>Show All</Text>
         </TouchableOpacity>
       </View>
 
@@ -33,8 +36,8 @@ const Popularjobs = () => {
           <Text>Something Went Wrong!</Text>
         ) : (
           <FlatList
-            data={[1, 2, 3, 4]}
-            renderItem={(item) => <PopularJobCard item={item} />}
+            data={data}
+            renderItem={({ item }) => <PopularJobCard item={item} />}
             keyExtractor={(item) => item?.job_id}
             contentContainerStyle={{ columnGap: SIZES.medium }}
             horizontal
